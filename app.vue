f194eb82d74ded1949c21e9029195db7cbf67c12
Muhamad Jamil <rezaramdhani461@gmail.com>
2024-11-08 00:57:25 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="h-screen">
+		<img src="/begin.jpg" alt="Vue logo" class="h-full w-full object-cover" />
+	</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+	title: "æthereal",
+});
+</script>

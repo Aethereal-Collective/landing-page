f46068055d5a8c7f0db6165820f2f962262e7ef2
Muhamad Jamil <rezaramdhani461@gmail.com>
2024-11-08 01:15:05 +0700

    ci: fix wrong port exposed

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker container stop aethereal-landing
+docker container rm aethereal-landing
 docker image rm aethereal-landing
 docker build -t aethereal-landing .
-docker run -d -p 4000:80 --restart unless-stopped --name aethereal-landing aethereal-landing
+docker run -d -p 4000:3000 --restart unless-stopped --name aethereal-landing aethereal-landing
